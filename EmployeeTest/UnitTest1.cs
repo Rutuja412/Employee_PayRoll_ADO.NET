8a9d7ad5475ab6a676a69b175c9520445a31a7e2
Rutuja412 <rutujakadam4545@gmail.com>
2023-02-19 00:14:24 +0530

    [Rutuja]:Add data  in EmployeePayroll using Stored Procedure

---
+++ EmployeeTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+using EmployeePayroll_ADO.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace EmployeeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            EmployeeRepo repo = new EmployeeRepo();
+            EmployeeModel employee = new EmployeeModel();
+            employee.EmployeeName = "Madhura";
+            employee.Department = "Technical";
+            employee.PhoneNumber = "6302956565";
+            employee.Address = "Chennai";
+            employee.Gender = 'F';
+            employee.BasicPay = 20000.00M;
+            employee.Deductions = 2500.00;
+            employee.StartDate = Convert.ToDateTime("2021-11-03");
+            //Mock<EmployeeRepo> mockObj = new Mock<EmployeeRepo>();
+            //mockObj.Setup(t=>t.AddEmployee(It.IsIn<EmployeeModel>)).return (true);
+            var result = repo.AddEmployee(employee);
+            Assert.IsTrue(result);
+        }
+    }
+}
--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,24 @@
 ﻿select * from Emp_Payroll
+create procedure [dbo].[SpAddEmployeeDetails]
+(
+@EmployeeName varchar(255),
+@PhoneNumber varchar(255),
+@Address varchar(255),
+@Department varchar(255),
+@Gender char(1),
+@BasicPay float,
+@Deductions float,
+@TaxablePay float,
+@Tax float,
+@NetPay float,
+@StartDate Date
+)
+as
+begin
+insert into Emp_Payroll values
+(
+@EmployeeName,@BasicPay,@StartDate,@Gender,@PhoneNumber,@Address,@BasicPay,@Department,@Deductions,@TaxablePay,@Tax,@NetPay
+)
+end
+GO
 
